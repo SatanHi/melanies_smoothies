432c278c8c83e6ed971a5e74d97b0389234c8b2f
Govind Pillai <131430264+SatanHi@users.noreply.github.com>
2024-05-22 15:57:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("Name on Smoothie will be: ", name_on_order)
 
-session = get_active_session()
 
 cnx = st.connection("snowflake")
 session = cnx.session()
